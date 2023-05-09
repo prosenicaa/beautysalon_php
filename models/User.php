fe92ce52313d2718e56cca5f0ad60645ad0f0fbf
prosenicaa <andjelaprosenica@gmail.com>
2023-05-09 20:38:40 +0200

    Azuriran User model

--- models/User.php
+++ models/User.php
@@ -20,8 +20,22 @@ class User
   
     public static function loginUser($user, mysqli $conn)
     {
-        $query = "SELECT * FROM user WHERE email='$user->email' and password='$user->password'";
+        $query = "SELECT * FROM user WHERE username='$user->username' and password='$user->password'";
         return $conn->query($query);
     }
+
+    public static function registerUser($user, mysqli $conn)
+    {
+        $query = "SELECT * FROM user WHERE email='$user->email'";
+        return $conn->query($query);
+    }
+    public static function addUser($user, $conn)
+    {
+        $query = "INSERT INTO user (username, email, password)
+        VALUES ('$user->username', '$user->email', '$user->password')";
+        return $conn->query($query);
+    }
+
+
 
 }
